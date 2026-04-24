4c65db246bb7d42f7c115434bf30ec7d13e08f1f
Phoenix2706 <phoenix270620@gmail.com>
2026-04-24 18:35:54 +0200

    feat: neo4j created and index updated

---
+++ src/config/neo4j.js
@@ -0,0 +1,14 @@
+const neo4j = require("neo4j-driver");
+let driver;
+function connectNeo4j() {
+  driver = neo4j.driver(
+    process.env.NEO4J_URI,
+    neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
+  );
+  console.log("Neo4j connected");
+}
+function getDriver() {
+  if (!driver) throw new Error("Neo4j not connected");
+  return driver;
+}
+module.exports = { connectNeo4j, getDriver };
---
+++ src/index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const { connectMongo } = require("./config/mongo");
+const { connectNeo4j } = require("./config/neo4j");
+const { connectRedis } = require("./config/redis");
+const app = express();
+app.use(express.json());
+app.use("/products", require("./routes/products"));
+app.use("/orders", require("./routes/orders"));
+app.use("/cart", require("./routes/cart"));
+app.use("/recommendations", require("./routes/recommendations"));
+async function start() {
+  await connectMongo();
+  connectNeo4j();
+  connectRedis();
+  app.listen(process.env.PORT, () => {
+    console.log(` API running on port ${process.env.PORT}`);
+  });
+}
+start();
