5855a06106eb93f561ffe5661037abc460edcc57
Jaimie <jaimie.mathangi78@gmail.com>
2026-04-25 16:58:30 +0200

    feat/reditseed

--- scripts/seed.js
+++ scripts/seed.js
@@ -26,6 +26,18 @@ async function seed() {
   await session.close();
   await driver.close();
 
+  // Redis
+
+  const redis = new Redis(process.env.REDIS_URL);
+  await redis.del("cart:user1");
+  await redis.hset("cart:user1",
+    "PASTE_P3_ID_HERE",
+    JSON.stringify({ quantity: 1, price: 39.99 })
+  );
+  await redis.expire("cart:user1", 604800);
+  console.log(" Redis seeded");
+  await redis.quit();
+
 }
 
 seed().catch(console.error);
