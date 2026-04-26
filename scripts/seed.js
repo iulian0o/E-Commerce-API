d64941cb10750bcd8adee79c89857d2f82a41451
Iulian Asanache <costi-iulian.asanache@epita.fr>
2026-04-26 10:33:20 +0200

    fix: Mongo in seed fixed

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,24 +1,29 @@
 require("dotenv").config();
 const neo4j = require("neo4j-driver");
 const Redis = require("ioredis");
-const { MongoClient, ObjectId } = require("mongodb");
 
 async function seed() {
+
   // Neo4j
   const driver = neo4j.driver(
     process.env.NEO4J_URI,
     neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
   );
   const session = driver.session();
-  await session.run("MATCH (n) DETACH DELETE n");
 
   // Mongo
+  const P1 = "PASTE_P1_ID_HERE"   // Wireless Headphones_id
+  const P2 = "PASTE_P2_ID_HERE"   // Mechanical Keyboard_id
+  const P3 = "PASTE_P3_ID_HERE"   // Laptop Stand_id
+
+  await session.run("MATCH (n) DETACH DELETE n");
+
   const purchases = [
-    { userId: "user1", productId: "PASTE_P1_ID_HERE" },
-    { userId: "user2", productId: "PASTE_P1_ID_HERE" },
-    { userId: "user2", productId: "PASTE_P2_ID_HERE" },
-    { userId: "user3", productId: "PASTE_P2_ID_HERE" },
-    { userId: "user3", productId: "PASTE_P3_ID_HERE" }
+    { userId: "user1", productId: P1 },
+    { userId: "user2", productId: P1 },
+    { userId: "user2", productId: P2 },
+    { userId: "user3", productId: P2 },
+    { userId: "user3", productId: P3 }
   ];
 
   for (const { userId, productId } of purchases) {
@@ -29,22 +34,23 @@ async function seed() {
       { userId, productId }
     );
   }
+
   console.log("Neo4j seeded");
   await session.close();
   await driver.close();
 
   // Redis
-
   const redis = new Redis(process.env.REDIS_URL);
+
   await redis.del("cart:user1");
   await redis.hset("cart:user1",
-    "PASTE_P3_ID_HERE",
+    P3,
     JSON.stringify({ quantity: 1, price: 39.99 })
   );
   await redis.expire("cart:user1", 604800);
-  console.log(" Redis seeded");
-  await redis.quit();
 
+  console.log("Redis seeded");
+  await redis.quit();
 }
 
 seed().catch(console.error);
