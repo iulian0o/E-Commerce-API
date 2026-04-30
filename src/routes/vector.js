3a73d2d8868e85563c6ba9d8679f61ece3792536
Iulian Asanache <costi-iulian.asanache@epita.fr>
2026-04-30 15:55:33 +0200

    fix: vectorServices imported in vector.js

--- src/routes/vector.js
+++ src/routes/vector.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { getSimilarProducts } = require("../services/vectorService");
+const { getSimilarProducts } = require("../services/vectoreServices");
 
 router.get("/:productId", async (req, res) => {
   try {
